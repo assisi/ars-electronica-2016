b2a7dff6f91f9881a6e60fcda79fa244d8ecca82
Damjan Miklic <damjan.miklic@gmail.com>
2016-08-18 12:36:47 +0200

    Working on AE Demo 1

--- fish-attract-bees/controllers/relay.py
+++ fish-attract-bees/controllers/relay.py
@@ -9,13 +9,21 @@ if __name__ == '__main__':
     context = zmq.Context(1)
     pub = context.socket(zmq.PUB)
     pub.bind('tcp://*:10103')
+    print('Publisher bound!')
 
-    sub = context.socket(zmq.SUB)
-    sub.connect('tcp://bbg-001:10201')
-    sub.connect('tcp://bbg-001:20202')
+    sub = context.socket(zmq.sub)
+    # Connect the address to listen to CATS
+    sub.connect('tcp://143.50.158.98:5557')
+    sub.setsockopt(zmq.SUBSCRIBE,'casu-001')
+    sub.setsockopt(zmq.SUBSCRIBE,'casu-002')
+    """
+    sub.connect('tcp://*:10201')
+    print('Connected left!')
+    sub.connect('tcp://*:20202')
+    print('Connected right!')
     sub.setsockopt(zmq.SUBSCRIBE, 'cats')
-
-    print('Everything is connected!')
+    """
+    print('Everything all subscribers!')
 
     while True:
         [name, msg, sender, data] = sub.recv_multipart()
