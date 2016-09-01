ad264bb5e16708c50bcd91c19b7196485348eabb
marcelo_ijk <marcelo.ijk@gmail.com>
2016-09-01 14:51:56 +0200

    Modifications after testing

--- fish-corridor-bees/controllers/communicate_and_heat.py
+++ fish-corridor-bees/controllers/communicate_and_heat.py
@@ -25,26 +25,37 @@ if __name__ == '__main__':
     myside = sys.argv[2]
     print('My side is {0}'.format(myside))
 
+    # Hardcode thresholds quick'n dirty
+    thresholds = {}
+    thresholds['left'] = [14000,11000,11000,11000,11000,10000]
+    thresholds['right'] = [11000,9000,14000,13000,10000,10000]
+
     while True:
-        msg = c.read_message()
-        c.sent_message('cats','0.5')
+        
+        # Estimate bee density
+        detected = [x > t for (x,t) in zip(c.get_ir_raw_value(casu.ARRAY),
+                                           thresholds[myside])]
+        estimate = float(sum(detected))/float(len(detected))
+        c.send_message('cats',str(estimate))
 
-        if msg:
-            print('Received: ' + msg['data'])
-            data = msg['data'].split(',')
-            count = 0
-            for item in data:
-                if item.split(':')[1] == myside:
-                    count += 1
-            if count == len(data):
-                # All fish are on our side -> heat up
-                temp_set = sorted([temp_min,temp_set+0.5,temp_max])[1]
-                c.set_temp(temp_set)
-                c.set_diagnostic_led_rgb(r = 1)
-            else:
-                # Cool down
-                temp_set = sorted([temp_min,temp_set-0.5,temp_max])[1]
-                c.set_temp(temp_set)
-                c.set_diagnostic_led_rgb(b=1)
+#        msg = c.read_message()
+#        if msg:
+#            print('Received: ' + msg['data'])
+#            data = msg['data'].split(',')
+#            count = 0
+#            for item in data:
+#                if item.split(':')[1] == myside:
+#                    count += 1
+#            if count == len(data):
+#                # All fish are on our side -> heat up
+#                temp_set = sorted([temp_min,temp_set+0.5,temp_max])[1]
+#                c.set_temp(temp_set)
+#                c.set_diagnostic_led_rgb(r = 1)
+#            else:
+#                # Cool down
+#                temp_set = sorted([temp_min,temp_set-0.5,temp_max])[1]
+#                c.set_temp(temp_set)
+#                c.set_diagnostic_led_rgb(b=1)
 
-        time.sleep(0.5)
+        time.sleep(1.0)
+        
